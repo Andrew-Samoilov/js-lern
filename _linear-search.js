fe1524c550a96c86409bf7646a4c172041858953
Andrew Samoilov <veprsag@gmail.com>
2021-09-07 23:07:40 +0300

    js from video

--- _linear-search.js
+++ _linear-search.js
@@ -1,14 +1,16 @@
-
+const array=[1, 4, 5, 8, 5, 1, 2, 7, 5, 2, 11]
+let count=0
 
 function linearSearch(array, item) {
-    const array=[1, 4, 5, 8, 5, 1, 2, 7, 5, 2, 11]
     for (let i = 0; i < array.lenght; i++){
-        document.write('--', i);
+        count+=1
         if (array[i]===item) {
-           document.write('--', i);
+           return i;
         }
     }
     return null;
 }
 
-linearSearch(array, 8);
+console.log(linearSearch(array, 8))
+console.log('count = ', count)
+

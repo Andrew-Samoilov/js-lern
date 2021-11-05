57ff63c1da8cf901725e64cb40a1bf8a8c1ea5c1
Andrew Samoilov <veprsag@gmail.com>
2021-11-05 15:26:48 +0200

    lern js

---
+++ js/hello-world.js
@@ -0,0 +1,23 @@
+let message = 'Привіт привіт привіт';
+alert(message);
+
+// comments like SaaS or ScSS
+
+let hello = 'Hello world!';
+
+
+// копируем значение 'Hello world' из переменной hello в переменную message
+message = hello;
+
+// теперь две переменные содержат одинаковые данные
+alert(hello); // Hello world!
+
+let admin;
+let names = 'Джон';
+admin = names;
+
+const OUR_PLANET = "Earth";
+
+// var min = 98;
+// document.getElementById("min").innerHTML = min;
+// ENd

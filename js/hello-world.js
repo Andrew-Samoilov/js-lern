fff199d292be2794e88d62460c3a5abadedc68c6
Andrew Samoilov <veprsag@gmail.com>
2021-11-22 18:02:00 +0200

    Update hello-world.js

--- js/hello-world.js
+++ js/hello-world.js
@@ -31,3 +31,11 @@ const message = `You ordered droids worth ${totalPrice} credits. Delivery (${del
 alert(`Привіт ${message}!`);
 
 const message = `You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFee} credits) is included in total price.`;
+
+function makeOrderMessage(orderedQuantity, pricePerDroid, deliveryFee) {
+  // Change code below this line
+  const totalU = orderedQuantity * pricePerDroid + deliveryFee;
+  const message = 'You ordered droids worth ${totalU} credits. Delivery (${deliveryFee} credits) is included in total price.';
+  // Change code above this line
+  return message;
+}

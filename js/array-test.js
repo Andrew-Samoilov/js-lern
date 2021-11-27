b74e753f45d2f1da485ed4fcae4cde89a4e271d7
Andrew Samoilov <veprsag@gmail.com>
2021-11-27 11:51:51 +0200

    --

--- js/array-test.js
+++ js/array-test.js
@@ -50,7 +50,6 @@ console.log(" - Array test - ");
 //     // Change code above this line
 // }
 
-
 // function calculateEngravingPrice(message, pricePerWord) {
 //     // Change code below this line
 //     return message.split(" ").length * pricePerWord;
@@ -62,13 +61,11 @@ console.log(" - Array test - ");
 // console.log(calculateEngravingPrice("Web-development is creative work", 43));
 // console.log(calculateEngravingPrice("Web-development is creative work", 21));
 
-
 // function slugify(title) {
 //     // Change code below this line
 //     // const resultSplit = title.toLowerCase().split(" ").join("-");
 //     // return resultSplit.join("-");
-//     return title.toLowerCase().split(" ").join("-");
-
+//     return title.toLowerCase().split(" ").join("-")
 
 //     // Change code above this line
 // }
@@ -76,7 +73,6 @@ console.log(" - Array test - ");
 // console.log(slugify("Arrays for begginers"));
 // console.log(slugify("English for developer"));
 // console.log(slugify("How to become a JUNIOR developer in TWO WEEKS"));
-
 
 // const oldClients = ['Mango', 'Ajax', 'Poly', 'Kiwi'];
 // const newClients = ['Peach', 'Houston'];
@@ -90,28 +86,83 @@ console.log(" - Array test - ");
 //     console.log(i);
 // }
 
-function calculateTotal(number) {
-    console.log('Number - ', number);
+// function calculateTotal(number) {
+//     console.log('Number - ', number);
+//     // Change code below this line
+//     let result = 0;
+//     if (number < 0) {
+//         result = 0;
+//     } else {
+//         for (let i = 0; i <= number; i++) {
+//             result += i;
+//             // console.log(" i ", i, ' result ', result);
+//         }
+//     }
+//     return result;
+//     // Change code above this line
+// }
+// console.log('-calculateTotal- ', calculateTotal(30));
+
+// console.log('-calculateTotal- ', calculateTotal(Math.random()));
+
+// const fruits = ['apple', 'plum', 'pear', 'orange'];
+
+
+// function calculateTotalPrice(order) {
+//     let total = 0;
+//     // console.log(" calculateTotalPrice ");
+
+//     // Change code below this line
+
+//     for (let i = 0; i < order.length; i++) {
+//         // const element = order[i];
+//         // console.log('i= ', i, '; order[i] ', order[i]);
+//         total += order[i];
+//     }
+
+//     // for (let i = 0; i <= order.lenght; i++) {
+//     //     console.log(' i=', i);
+//     //     total += order[i];
+//     // }
+//     // // Change code above this line
+//     return total;
+// }
+
+// console.log(calculateTotalPrice([12, 85, 37, 4]));
+// console.log(calculateTotalPrice([164, 48, 291]));
+// console.log(calculateTotalPrice([412, 371, 94, 63, 176]));
+
+function findLongestWord(string) {
     // Change code below this line
-    let result = 0;
-    if (number < 0) {
-        result = 0;
-    } else {
-        for (let i = 0; i <= number; i++) {
-            result += i;
-            // console.log(" i ", i, ' result ', result);
-        }
+    console.log(" findLongestWord ");
+    if (!string) {
+        return 0;
     }
+
+    const stringArray = string.split(" ");
+    console.table(stringArray);
+    let maxLength = 0;
+    let result = '';
+
+    for (let i = 0; i < stringArray.length; i++) {
+
+        if (maxLength < stringArray[i].length) {
+            maxLength = stringArray[i].length;
+            result = stringArray[i];
+            console.log('- in if- ', maxLength, ' word ', result);
+        }
+
+    }
+
     return result;
+
     // Change code above this line
 }
-console.log('-calculateTotal- ', calculateTotal(30));
 
-console.log('-calculateTotal- ', calculateTotal(Math.random()));
+console.log(findLongestWord("The quick brown fox jumped over the lazy dog"));
+console.log(findLongestWord("Google do a roll"));
+console.log(findLongestWord("May the force be with you "));
+console.log(findLongestWord());
 
-const fruits = ['apple', 'plum', 'pear', 'orange'];
 
-for (let i = i , i < planets.length, i++) { // Change this line
-  const fruit = fruits[]; // Change this line
-  console.log(fruit[i]);
-}
+console.log();

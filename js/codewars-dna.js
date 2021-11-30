1d5b47e01082ce952ff03fd80cd592e247a74842
Andrew Samoilov <veprsag@gmail.com>
2021-11-30 17:43:12 +0200

    codewars dna

--- js/codewars-dna.js
+++ js/codewars-dna.js
@@ -2,15 +2,36 @@ console.log(" - * Code wars * - ");
 console.log();
 console.log();
 
-function DNAStrand(dna){
+function DNAStrand(dna) {
+ 
   //your code here
-  const args = Array.from(arguments[0]);
-  console.log('-', args);
+  if (!dna[0]) { return []; }
+
+  const args = Array.from(arguments);
+  // console.log('-', args);
+  const result = args[0].split("")||[];
+  // console.log('- split', result);
+
+  for (let i = 0; i < result.length; i++) {
+    // console.log(`- result[${i}] ${result[i]}`);
+    switch (result[i]) {
+      case 'A': result[i] = 'T'; break;
+      case 'C': result[i] = 'G'; break;
+      case 'T': result[i] = 'A'; break;
+      case 'G': result[i] = 'C'; break;    
+      
+      default: result[i] = 'Z'; break;
+    }
+  }
+//  console.log('- after', result.join(''));
+
+  return result.join('');
 }
 
 
 
-// console.log(DNAStrand([]));
-console.log(DNAStrand(['ATGC']));
-console.log(DNAStrand(['GTAT']));
-console.log(DNAStrand(['AAAA']));
+console.log(DNAStrand([]));
+console.log(DNAStrand('AAA'));
+console.log(DNAStrand('ATGC'));
+console.log(DNAStrand('GTATa'));
+
--- js/test4.js
+++ js/test4.js
@@ -1,24 +1,3 @@
-console.log("Open or Senior - codewars");
-// codewars - open or senior
-// 1а задача приблизно - допоможіть крікетному клубу розсортувати учасників. "Сеньйором" вважаєтья
-// учасник старше 55 років і з кількістю "гандікапів" 7 (умовно, там чогось іншого). Інші - open.
-
-function openOrSenior(age, handicap) {
-    // варіант 1 - найпростіше в лоб
-    // if (age > 54 && handicap > 6) {
-    //     return "Senior";
-    // } else {
-    //     return "Open";
-    // }
-    return (age > 54 && handicap > 6) ? "Senior" : "Open";
-}
-
-console.log(openOrSenior(44, 32));
-console.log(openOrSenior(55, 3));
-console.log(openOrSenior(45, 1));
-console.log(openOrSenior(55, 21));
-
-
 
 for (let i = 2; i < 6; i++) {
     console.log(i);

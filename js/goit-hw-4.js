d561334aba1469f7ee0436a4dc254e644135ab21
Andrew Samoilov <veprsag@gmail.com>
2021-12-10 15:47:02 +0200

    hw4 in-progress

--- js/goit-hw-4.js
+++ js/goit-hw-4.js
@@ -3,6 +3,36 @@ console.log();
 console.log();
 
 
+console.log('---==*==---');
+// ----------
+const pizzaPalace = {
+    pizzas: ['Ultracheese', 'Smoked', 'Four meats'],
+    order(pizzaName, onSuccess, onOrderError) {
+        // console.log(this.pizzas.indexOf(pizzaName));
+        if (this.pizzas.indexOf(pizzaName) === -1) {
+            return onOrderError(`There is no pizza with a name ${pizzaName} in the assortment.`)
+        }
+        return makePizza(pizzaName);
+    },
+};
+// Change code above this line
+
+// Callback for onSuccess
+function makePizza(pizzaName) {
+    return `Your order is accepted. Cooking pizza ${pizzaName}.`;
+}
+
+// Callback for onError
+function onOrderError(error) {
+    return `Error! ${error}`;
+}
+
+// Method calls with callbacks
+console.log(pizzaPalace.order('Smoked', makePizza, onOrderError));
+console.log(pizzaPalace.order('Four meats', makePizza, onOrderError));
+console.log(pizzaPalace.order('Big Mike', makePizza, onOrderError));
+console.log(pizzaPalace.order('Vienna', makePizza, onOrderError));
+
 
 
 console.log('---==*==---');

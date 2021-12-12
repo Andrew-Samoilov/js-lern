74db7f36f2ad53821964e9905410f244f20defed
Andrew Samoilov <veprsag@gmail.com>
2021-12-12 15:06:10 +0200

    +задачка 7го рівня за 20хв

--- js/codewars-sum-of-num.js
+++ js/codewars-sum-of-num.js
@@ -7,16 +7,30 @@ var SequenceSum = (function () {
     function SequenceSum() { }
 
     SequenceSum.showSequence = function (count) {
+        if (count < 0) {
+            return `${count}<0`;
+        };
+        if (count === 0) {
+            return `0=0`;
+        };
 
+        let resultStr = '0';
+        let resultInt = 0;
+        // console.log(count);
+        for (let i = 1; i < count + 1; i++) {
+            resultStr += `+${i}`;
+            resultInt += i;
+            // console.log(resultStr, resultInt);
+        }
+        return resultStr + ' = ' + resultInt;
         // for
         //while
     };
 
     return SequenceSum;
-
 })();
 
 
-console.log(SequenceSum.showSequence(-5), "-5");
-console.log(SequenceSum.showSequence(0), "00 = 0");
-console.log(SequenceSum.showSequence(6), "0+1+2+3+4+5+6 = 21");
+console.log(SequenceSum.showSequence(-5), "| -5");
+console.log(SequenceSum.showSequence(0), "| 00 = 0");
+console.log(SequenceSum.showSequence(6), " | 0+1+2+3+4+5+6 = 21");

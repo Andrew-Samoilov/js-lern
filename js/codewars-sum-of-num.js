864603e76f67243d9982d39475662e808e37ce49
Andrew Samoilov <veprsag@gmail.com>
2021-12-12 14:13:58 +0200

    -

---
+++ js/codewars-sum-of-num.js
@@ -0,0 +1,22 @@
+console.log(" - * Code wars. Sum of numbers from 0 to N * - ");
+console.log();
+console.log();
+
+
+var SequenceSum = (function () {
+    function SequenceSum() { }
+
+    SequenceSum.showSequence = function (count) {
+
+        // for
+        //while
+    };
+
+    return SequenceSum;
+
+})();
+
+
+console.log(SequenceSum.showSequence(-5), "-5");
+console.log(SequenceSum.showSequence(0), "00 = 0");
+console.log(SequenceSum.showSequence(6), "0+1+2+3+4+5+6 = 21");

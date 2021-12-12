a09a5d10eeae453f3073c08d5dd68a05ac7b1f43
Andrew Samoilov <veprsag@gmail.com>
2021-12-12 22:08:59 +0200

    cw conwert 2 human time - done!!
    На зараз - найскладніше ката.
    Як завжди :) написав parseInt.
    Бест практіси прям треба розбирати до кісток, як працює.

--- js/cw-human-time2.js
+++ js/cw-human-time2.js
@@ -3,24 +3,53 @@ console.log();
 console.log();
 
 function humanReadable(seconds) {
-    let how = 0;
-    function howCount(sec) {
+    console.log('1| Sec', seconds);
+    function hourCount(sec) {
         // рахуємо години
+        let hour = 0;
         if (sec < 3600) { return '00' }
-        how = (seconds - (seconds % 3600)) / 3600;
-        // console.log('Hour-', how);
-        if (how < 10) {
-            return `0${how}`
+        hour = (seconds - (seconds % 3600)) / 3600;
+        // console.log('Hour-', hour);
+        if (hour < 10) {
+            seconds -= hour * 3600;
+            console.log('2| Sec - ', seconds);
+            return `0${hour}`
         }
-        return how;
+        seconds -= hour * 3600;
+        console.log('3| Sec -', seconds);
+        return hour;
     }
 
+    function minCount(sec) {
+        // рахуємо хвилини
+        let min = 0;
+        console.log('4| min', sec);
+        if (sec < 60) { return '00' }
+        min = ((seconds - (seconds % 60)) / 60);
+        console.log('5| Min-', min);
+        if (min < 10) {
+            seconds -= min * 60;
+            console.log('6| Min - ', seconds);
+            return `0${min}`;
+        }
+        seconds -= min * 60;
+        console.log('7| Min - ', seconds);
+        return min;
+    }
 
-    // console.log('Hour % 3600', (seconds % 3600));
+    function secCount(sec) {
+        // cекунди
+        console.log('8| sec', sec);
+        if (sec === 0) { return '00' }
+        if (sec < 10) {
+            console.log('9| Min - ', sec);
+            return `0${sec}`;
+        }
+        console.log('0| Min - ', sec);
+        return sec;
+    }
 
-
-
-    return `${howCount(seconds)}:`;
+    return `${hourCount(seconds)}:${minCount(seconds)}:${secCount(seconds)}`;
 }
 
 
@@ -28,8 +57,8 @@ function humanReadable(seconds) {
 console.log('00:00:00|', humanReadable(0));
 // console.log('00:00:59|', humanReadable(59));
 // console.log('00:01:00|', humanReadable(60));
-// console.log('00:59:59|', humanReadable(3599));
-// console.log('01:00:00|', humanReadable(3601));
+console.log('00:59:59|', humanReadable(3599));
+// console.log('01:00:00|', humanReadable(3600));
 console.log('12:34:56|', humanReadable(45296));
 // console.log('24:00:00|', humanReadable(86400));
 console.log('99:59:59|', humanReadable(359999));

78701e88cbd7ff8980989d608264075ece803c88
Andrew Samoilov <veprsag@gmail.com>
2021-12-20 12:06:53 +0200

    hw5 in progress

--- js/goit-hw-5-done.js
+++ js/goit-hw-5-done.js
@@ -1,7 +1,29 @@
 {
+    // 10
+    class Storage {
+        constructor([...items]) {
+            this.items = items;
+        }
+        getItems() {
+            return this.items;
+        }
+        addItem(newItem) {
+            this.items.push(newItem);
+        }
+        removeItem(itemToRemove) {
+            this.items.splice(this.items.indexOf(itemToRemove), 1);
+        }
+    }
 
-    
+    // Change code above this line
+    const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+    console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+    storage.addItem("Droid");
+    console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+    storage.removeItem("Prolonger");
+    console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
 }
+
 {
     // 2
     const customer = {

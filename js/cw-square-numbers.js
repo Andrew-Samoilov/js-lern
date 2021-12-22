0499766f50e79b0fd56e6b4c3c6770b666a86e95
Andrew Samoilov <veprsag@gmail.com>
2021-12-22 22:36:29 +0200

    ion progress

--- js/cw-square-numbers.js
+++ js/cw-square-numbers.js
@@ -4,35 +4,51 @@ console.log();
 // https://www.codewars.com/kata/5edc8c53d7cede0032eb6029/train/javascript
 
 function solve(n) {
-    // console.log('Inside func', n);
+    console.log('Inside func', n);
     function isSquare(num) {
+        if (num > 100) return num + 'too mach';
         // console.log(`Math.sqrt(n2) ${Math.sqrt(n2)}, Math.trunc(Math.sqrt(n2)) ${Math.trunc(Math.sqrt(n2))}`);
-        return (Math.sqrt(n2) === Math.trunc(Math.sqrt(n2)));
+        return (Math.sqrt(num) === Math.trunc(Math.sqrt(num)));
     };
 
     let n2 = 0;
 
-    let n3 = 0;
-    // if (n <= 2) return -1;
-    n2 = n;
-    console.log('n2', n2);
+    n2 = 1;
 
-
-    if (Math.sqrt(n2) === Math.trunc(Math.sqrt(n2))) return 0;
-
-    n2 = Math.sqrt(n2)
-    console.log('Math.sqrt', n2);
-    n2 = Math.trunc(n2);
-    console.log('Math.trunc', n2);
-
-    n2 = Math.trunc(n2) + 1;
-    console.log('n2', n2, 'n2*n2', n2 * n2);
     console.log(`isSquare ${n2} ${isSquare(n2)}`);
+    console.log(`isSquare ${n} + ${n2} ${isSquare(n + n2)}`);
+    while (isSquare(n2) && isSquare(n + n2)) {
+        console.log('Inside do while - n2', n2);
+        n2++;
+    }
 
-    return n2 * n2 - n;
+    // do {
+    //     console.log('Inside do while - n2', n2);
+    //     n2++;
+    // } while (isSquare(n2) && isSquare(n + n2));
+
+    // console.log('Squere n2 =', n2);
+    // console.log(`isSquare ${n} + ${n2}  ${isSquare(n + n2)}`);
+
+    // console.log(`isSquare ${n2} ${isSquare(n2)}`);
+    // console.log('Math.sqrt', Math.sqrt(n2));
+
+
+    // if (Math.sqrt(n2) === Math.trunc(Math.sqrt(n2))) return 0;
+
+    // n2 = Math.sqrt(n2)
+    // console.log('Math.sqrt', n2);
+    // n2 = Math.trunc(n2);
+    // console.log('Math.trunc', n2);
+
+    // n2 = Math.trunc(n2) + 1;
+    // console.log('n2', n2, 'n2*n2', n2 * n2);
+    // console.log(`isSquare ${n2} ${isSquare(n2)}`);
+
+    return n2;
 }
 
-console.log(solve(1));
+console.log(solve(3));
 
 // assert.strictEqual(solve(3), 1)
 // assert.strictEqual(solve(4), -1)

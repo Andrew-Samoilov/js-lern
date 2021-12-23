04c92546d14610da15646ccca0d6659b2d6e9f05
Andrew Samoilov <veprsag@gmail.com>
2021-12-23 16:59:00 +0200

    square numbers done!

--- js/cw-square-numbers.js
+++ js/cw-square-numbers.js
@@ -5,41 +5,61 @@ console.log();
 
 function solve(n) {
     console.log('Inside func', n);
+
+    let delta = 3;
     let counter = 0;
+    const maxCount = n / 2 + 1;
+
+    // console.log('------- дивимся на квадратні номери потім шукаємо дельту');
+    // let previousValue = 0;
+    // for (let index = 1; index < 1200; index++) {
+    //     if (isSquare(index)) {
+    //         console.log(`Square num ${index}, +${index - previousValue}`);
+    //         previousValue = index;
+    //     }
+    // }
+    // console.log('-------');
+
     function isSquare(num) {
-        if (num > 1000) return num + 'too mach';
-        // console.log(`Math.sqrt(n2) ${Math.sqrt(n2)}, Math.trunc(Math.sqrt(n2)) ${Math.trunc(Math.sqrt(n2))}`);
-        return (Math.sqrt(num) === Math.trunc(Math.sqrt(num)));
+        const n = Math.sqrt(num);
+        return (n === Math.trunc(n));
     };
 
     let n2 = 1;
 
-    // console.log(`isSquare ${n2} ${isSquare(n2)}`);
-    // console.log(`isSquare ${n} + ${n2} ${isSquare(n + n2)}`);
+    console.log(`isSquare ${n2} ${isSquare(n2)}`);
+    console.log(`isSquare ${n} + ${n2} ${isSquare(n + n2)}`);
+
     let is2Square = false;
 
     do {
         // console.log('Inside do while - n2', n2);
-        // console.log(`isSquare ${n} + ${n2} ${isSquare(n + n2)}`);
         if (isSquare(n2)) {
-            // console.log(`_Inside 1 if, isSquare ${n2} ${isSquare(n2)}`);
+            // console.log('_   Inside 1st if - n2', n2);
             if (isSquare(n + n2)) {
-                console.log(`__! Inside 2 if, isSquare ${n} + ${n2} ${isSquare(n + n2)}, counter = ${counter}`);
+                console.log(`! Inside 2 if, isSquare ${n} + ${n2} ${isSquare(n + n2)}, counter = ${counter}`);
                 is2Square = true;
-
                 return n2;
             };
-        };
-        n2++;
+        }
+        // console.log(`n2, ${n2}`, 'delta', delta);
+
+        n2 += delta;
+
+        delta = Math.trunc(Math.sqrt(n2) * 2 + 1);
+        // console.log(` n2, ${n2}`);
         counter++;
+
+        if (counter > maxCount) {
+            console.log(`maxCount !, isSquare ${n} + ${n2} ${isSquare(n + n2)}, counter = ${counter}`);
+            return -1;
+        }
     } while (!is2Square);
 
-    console.log(`After while isSquare ${n} + ${n2} ${isSquare(n + n2)}`);
-
-    return n2;
 }
 
-console.log(solve(17));
+
+console.log(solve(3));
 
 // assert.strictEqual(solve(3), 1)
 // assert.strictEqual(solve(4), -1)
@@ -51,5 +71,5 @@ console.log(solve(17));
 // assert.strictEqual(solve(11), 25)
 // assert.strictEqual(solve(13), 36)
 // assert.strictEqual(solve(17), 64)
-// assert.strictEqual(solve(88901), 5428900)
-// assert.strictEqual(solve(290101), 429235524)
+// assert.strictEqual(solve(88901), 5428900)  console.log(solve(8356730));
+// assert.strictEqual(solve(290101), 429235524) 

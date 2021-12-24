f24fdff452c86c7e275f35c4ed67b91d25431caf
Andrew Samoilov <veprsag@gmail.com>
2021-12-24 14:43:53 +0200

    Update cw-snail.js

--- js/cw-snail.js
+++ js/cw-snail.js
@@ -5,8 +5,19 @@ console.log('   S');
 
 function snail(array) {
     console.table(array);
-    console.log('length', array[[array]]);
+    console.log(`Arr length ${array.length} * ${array[0].length} = `, array.length * array[0].length);
+    let x = 0;
+    let y = 0;
+    let deltaX = 1;
+    let deltaY = 0;
+
     const snailArray = [];
+    for (let i = 0; i < array.length * array[0].length; i++) {
+        console.log('-', array[y][x]);
+        x += deltaX;
+        y += deltaY;
+
+    }
     return snailArray;
 }
 
@@ -18,10 +29,7 @@ const arr = [
 
 console.log(snail(arr));
 
-console.log('arr[0][0]', arr[0][0]); // 1
-console.log('arr[0][1]', arr[0][1]); // 2
-console.log('arr[0][2]', arr[0][2]); // 3
-console.log('arr[1][2]', arr[1][2]); // 4
-
-
-
+// console.log('arr[0][0]', arr[0][0]); // 1
+// console.log('arr[0][1]', arr[0][1]); // 2
+// console.log('arr[0][2]', arr[0][2]); // 3
+// console.log('arr[1][2]', arr[1][2]); // 4

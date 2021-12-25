34f2f715cf7be5c5df64ecb5420505b3f1bf44ee
Andrew Samoilov <veprsag@gmail.com>
2021-12-25 20:20:33 +0200

    snail

--- js/cw-snail.js
+++ js/cw-snail.js
@@ -4,9 +4,31 @@ console.log('W  +  E            |');
 console.log('   S               y');
 
 function snail(array) {
-    // start init
     console.table(array);
-    const snailArray = [];
+
+    // start init
+    function addZeroFrame(array) {
+        array.unshift([0]);
+        array.push([0]);
+
+        for (let i = 0; i < array.length - 2; i++) {
+            array[0][i] = 'x';
+            array[array.length - 1][i] = 'x';
+        }
+        // console.table(array);
+
+        for (let i = 0; i < array.length; i++) {
+            for (let j = 0; j < array[1].length; j++) {
+                // console.log(` array[${i}][${j}] ${array[i][j]}`);
+            }
+            array[i].unshift('x');
+            array[i].push('x');
+        }
+        // console.table(array);
+    }
+    addZeroFrame(array);
+    console.table(array);
+    const snailArray = [array[1][1]];
     console.log(`Arr length ${array.length} * ${array[0].length} = `, array.length * array[0].length);
     let y = 1;
     let x = 1;
@@ -18,8 +40,8 @@ function snail(array) {
     let deltaY = 0;
     let deltaX = 1;
     let tmplValue = array[y][x];
-    console.log(`Start direction ${direction}, dY=${deltaY} dX=${deltaX}`);
-    console.log(`Start position, array[${y}][${x}]-`, array[y][x],);
+    // console.log(`Start direction ${direction}, dY=${deltaY} dX=${deltaX}`);
+    // console.log(`Start position, array[${y}][${x}]-`, array[y][x],);
     // start init
 
     // North, South, East, West
@@ -29,25 +51,25 @@ function snail(array) {
                 direction = `South`;
                 deltaY = 1;
                 deltaX = 0;
-                console.log(` New direction: ${direction}, dY=${deltaY} dX=${deltaX}`);
+                // console.log(` New direction: ${direction}, dY=${deltaY} dX=${deltaX}`);
                 break;
             case 'South':
                 direction = `West`;
                 deltaY = 0;
                 deltaX = -1;
-                console.log(` New direction: ${direction}, dY=${deltaY} dX=${deltaX}`);
+                // console.log(` New direction: ${direction}, dY=${deltaY} dX=${deltaX}`);
                 break;
             case 'West':
                 direction = `North`;
                 deltaY = -1;
                 deltaX = 0;
-                console.log(`New direction: ${direction}, dY=${deltaY} dX=${deltaX}`);
+                // console.log(`New direction: ${direction}, dY=${deltaY} dX=${deltaX}`);
                 break;
             case 'North':
                 direction = `East`;
                 deltaY = 0;
                 deltaX = 1;
-                console.log(`  New direction: ${direction}, dY=${deltaY} dX=${deltaX}`);
+                // console.log(`  New direction: ${direction}, dY=${deltaY} dX=${deltaX}`);
                 break;
             default:
                 break;
@@ -63,19 +85,25 @@ function snail(array) {
         // next step
         yNew = y + deltaY;
         xNew = x + deltaX;
-        array[y][x] = 0;
+        array[y][x] = 'x';
         // console.log(`old=0, array[${y}][${x}]-`, array[y][x],);
 
-        if (array[yNew][xNew] === 0) {
-            console.log(`Find 0, array[${yNew}][${xNew}], ${array[yNew][xNew]} change direction...`);
+        if (array[yNew][xNew] === 'x') {
+            // console.log(`Find 0, array[${yNew}][${xNew}], ${array[yNew][xNew]} change direction...`);
             changeDirection(direction);
         } else {
             y = yNew;
             x = xNew;
         }
-        console.log(`_ ${direction}, array[${y}][${x}] - `, array[y][x],);
+        if (array[y][x] != 'x') {
+            snailArray.push(array[y][x]);
+
+        }
+        // console.log(`_ ${direction}, array[${y}][${x}] - `, array[y][x],);
     }
     return snailArray;
+    if (array[[1]] = 1) return 1;
+    if (array[[]] = []) return [];
 }
 // const arr = [
 //     [0, 0, 0, 0, 0],
@@ -85,41 +113,19 @@ function snail(array) {
 //     [0, 0, 0, 0, 0]
 // ];
 
+// const arr = [
+//     [1, 2, 3],
+//     [8, 9, 4],
+//     [7, 6, 5]
+// ];
 const arr = [
-    [1, 2, 3],
-    [8, 9, 4],
-    [7, 6, 5]
+    [1, 2],
+    [4, 3]
 ];
+// const arr = [[1]];
 
-function addZeroFrame(array) {
-
-    // array.unshift(0);
-    // array.push(0);
-    console.table(array);
-
-
-    for (let i = 0; i < array.length; i++) {
-        for (let j = 0; j < array[1].length; j++) {
-            console.log(` array[${i}][${j}] ${array[i][j]}`);
-            // if (array[i][j] == null) {
-            //     array[i][j] =0;
-            // }
-
-
-        }
-        array[i].unshift(0);
-        array[i].push(0);
-    }
-
-    // array.unshift('5');
-    array.push(9);
-    // array.push(0);
-
-    console.table(array);
-}
-
-console.log('Add zero frame ', addZeroFrame(arr));
-// console.log('go ', snail(arr));
+// console.log('Add zero frame ', addZeroFrame(arr));
+console.log('go ', snail(arr));
 
 // console.log('arr[2][3]', arr[2][3]);
 // console.log('arr[0][0]', arr[0][0]); // 1

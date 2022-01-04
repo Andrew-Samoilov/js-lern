e0a99465685a82015d2954a43abe73b945305a64
Andrew Samoilov <veprsag@gmail.com>
2022-01-04 12:51:13 +0200

    +

--- js/test.js
+++ js/test.js
@@ -1,3 +1,23 @@
 console.log(" -  * test * - ");
 console.log(" -  * CodeWars *  * - ");
 console.log();
+
+{
+    const add = (initValue) => {
+        let result = initValue;
+
+        const addRecursive = (value) => {
+            if (!value) return result;
+
+            result+=value;
+
+            return addRecursive;
+        }
+    return addRecursive;
+    }
+
+const res-add(1)(2)(54)();
+console.log(res);
+
+
+}

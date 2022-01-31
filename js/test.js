f1ae1e2ff538a55f3ed1fbd6823197572fb1b11b
Andrew Samoilov <veprsag@gmail.com>
2022-01-31 22:34:33 +0200

    Pron treasures

--- js/test.js
+++ js/test.js
@@ -1,4 +1,25 @@
-console.log(" -  * test * - ");
-console.log(" -  * CodeWars *  * - ");
+console.log(' -  * test * - ');
+console.log(' -  * CodeWars *  * - ');
 console.log();
 
+function pron(inputX, inputY) {
+    console.log(`x ${inputX}, y ${inputY}`);
+    let num = 1;
+    let x = 1;
+
+    for (let i = 0; i < 1000000; i++) {
+        console.log(`i ${i}`);
+        let x2 = x;
+        console.log(`x2 ${x2}`);
+        for (let y = 1; y <= x; y++) {
+            console.log(`x ${x2} y${y} num ${num}`);
+            if (x2 === inputX && y === inputY) return num;
+            num++;
+            x2--;
+        }
+        x++;
+    }
+}
+
+console.log(pron(10, 5));
+console.log(pron(2, 3));

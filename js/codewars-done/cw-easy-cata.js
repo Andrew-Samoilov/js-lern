bc256041c976d1410972807bd411aac62b9b3c42
Andrew Samoilov <veprsag@gmail.com>
2022-02-04 14:29:52 +0200

    cw

--- js/codewars-done/cw-easy-cata.js
+++ js/codewars-done/cw-easy-cata.js
@@ -1,5 +1,14 @@
 console.log(" -  * CodeWars * - ");
 console.log();
+{
+    const reverseSeq = n => {
+        const res = [];
+        for (let i = n; i > 0; i--) res.push(i);
+        return res;
+    };
+
+    console.log(reverseSeq(5), [5, 4, 3, 2, 1]);
+}
 
 {
     function findNeedle(haystack) {

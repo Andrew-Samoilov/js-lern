e9b4ffcc98acedae10a380bbc21bb1926fc1a2ba
Andrew Samoilov <veprsag@gmail.com>
2022-02-21 23:14:45 +0200

    Update _bnc.js

--- js/_bnc.js
+++ js/_bnc.js
@@ -1,23 +1,15 @@
 console.log(' -  * Bnc API *  * - ');
 
-let myHeaders = new Headers();
-myHeaders.append("Content-Type", "application/json", 'Access-Control-Allow-Headers: *');
+var myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
 
-let requestOptions = {
+var requestOptions = {
     method: 'GET',
     headers: myHeaders,
-    mode: 'cors',
     redirect: 'follow'
 };
-// let res = '';
+
 fetch("https://testnet.binance.vision/api/v3/ticker/24hr", requestOptions)
     .then(response => response.text())
     .then(result => console.log(result))
     .catch(error => console.log('error', error));
-
-// fetch("https://testnet.binance.vision/api/v3/ticker/24hr", requestOptions)
-//     .then(response => response.text())
-//     .then(result => res = result)
-//     .catch(error => console.log('error', error));
-
-// console.log(res);

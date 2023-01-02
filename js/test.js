faeeb4b2daa911a704991a579aa4360adc89e630
Andrew Samoilov <veprsag@gmail.com>
2023-01-02 22:27:11 +0200

    Update test.js

--- js/test.js
+++ js/test.js
@@ -1,7 +1,33 @@
 console.log(' -  * test * - ');
 console.log(' -  * CodeWars *  * - ');
-const productName = "Droid";
-const pricePerItem = 3500;
-const message = `You picked ${productName}, price per item is ${pricePerItem} credits`;
 
-console.log(message);
+// 1.13 avtotest
+// function isAdult(age) {
+//     // Change code below this line
+//     // const passed = age >= 18 ? true : false; // without () also works
+//     const passed = (age >= 18) ? true : false;
+//     // Change code above this line
+//     return passed;
+// }
+// function isAdult(age) {
+//     // Change code below this line
+//     let passed = false;
+//     if (age >= 18) {
+//         passed = true;
+//     }
+//     // Change code above this line
+//     return passed;
+// }
+// console.log(isAdult(20));
+// console.log(isAdult(14));
+// console.log(isAdult(8));
+// console.log(isAdult(37));
+// 1.25 autotest dont work
+// function checkStorage(available, ordered) {
+//     let message;
+//     // Change code below this line
+//     ordered > available ? message = "Not enough goods in stock!" : message = "The order is accepted, our manager will contact you"
+//     // Change code above this line
+//     return message;
+// }
+

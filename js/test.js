61102803724425ab8ec80c9093e82b3e4f266aea
Andrew Samoilov <veprsag@gmail.com>
2023-01-03 18:33:48 +0200

    Update test.js

--- js/test.js
+++ js/test.js
@@ -31,3 +31,11 @@ console.log(' -  * CodeWars *  * - ');
 //     return message;
 // }
 
+const fruits = ["apple", "plum", "pear", "orange"];
+
+// Change code below this line
+const firstElement = fruits[0];
+const secondElement = fruits[1];
+const lastElement = fruits[fruits.length - 1];
+
+console.log(lastElement);

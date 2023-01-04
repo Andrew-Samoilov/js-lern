92d45d1d0d97ed0ae56cb70079eed20207c58604
Andrew Samoilov <veprsag@gmail.com>
2023-01-04 22:20:11 +0200

    hw3 autotest

--- js/test.js
+++ js/test.js
@@ -1,16 +1,26 @@
 console.log(' -  * test * - ');
 // console.log(' -  * CodeWars *  * - ');
+// 3.19
+const products = [
+    { name: "Radar", price: 1300, quantity: 4 },
+    { name: "Scanner", price: 2700, quantity: 3 },
+    { name: "Droid", price: 400, quantity: 7 },
+    { name: "Grip", price: 1200, quantity: 9 },
+];
 
-const book = {
-    title: "The Last Kingdom",
-    author: "Bernard Cornwell",
-    genres: ["historical prose", "adventure"],
-    rating: 8.38,
-};
+function getAllPropValues(propName) {
+    // Change code below this line
+    const result = [];
+    for (const product of products) {
+        if (product.hasOwnProperty(propName)) {
+            console.log(product[propName]);
+            result.push(product[propName]);
+        }
+    }
+    return result;
 
-for (const key in book) {
-    // Ключ
-    console.log(key, book[key]);
-    // Значення властивості з таким ключем
-
+    // Change code above this line
 }
+
+console.log(getAllPropValues("price"), [1300, 2700, 400, 1200]);
+console.log(getAllPropValues("category"), []);

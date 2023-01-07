3ee4d65d5bcb81618b04de07b785e15f282fe7b9
Andrew Samoilov <veprsag@gmail.com>
2023-01-07 14:42:00 +0200

    Update test.js

--- js/test.js
+++ js/test.js
@@ -1,87 +1,25 @@
 console.log(' -  * test * - '); // console.log(' -  * CodeWars *  * - ');
-const users = [
+
+const books = [
     {
-        name: "Moore Hensley",
-        email: "moorehensley@indexia.com",
-        eyeColor: "blue",
-        friends: ["Sharron Pace"],
-        isActive: false,
-        balance: 2811,
-        skills: ["ipsum", "lorem"],
-        gender: "male",
-        age: 37,
+        title: "The Last Kingdom",
+        author: "Bernard Cornwell",
+        genres: ["adventure", "history"],
     },
     {
-        name: "Sharlene Bush",
-        email: "sharlenebush@tubesys.com",
-        eyeColor: "blue",
-        friends: ["Briana Decker", "Sharron Pace"],
-        isActive: true,
-        balance: 3821,
-        skills: ["tempor", "mollit", "commodo", "veniam", "laborum"],
-        gender: "female",
-        age: 34,
+        title: "Beside Still Waters",
+        author: "Robert Sheckley",
+        genres: ["fiction", "mysticism"],
     },
     {
-        name: "Ross Vazquez",
-        email: "rossvazquez@xinware.com",
-        eyeColor: "green",
-        friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
-        isActive: false,
-        balance: 3793,
-        skills: ["nulla", "anim", "proident", "ipsum", "elit"],
-        gender: "male",
-        age: 24,
-    },
-    {
-        name: "Elma Head",
-        email: "elmahead@omatom.com",
-        eyeColor: "green",
-        friends: ["Goldie Gentry", "Aisha Tran"],
-        isActive: true,
-        balance: 2278,
-        skills: ["adipisicing", "irure", "velit"],
-        gender: "female",
-        age: 21,
-    },
-    {
-        name: "Carey Barr",
-        email: "careybarr@nurali.com",
-        eyeColor: "blue",
-        friends: ["Jordan Sampson", "Eddie Strong"],
-        isActive: true,
-        balance: 3951,
-        skills: ["ex", "culpa", "nostrud"],
-        gender: "male",
-        age: 27,
-    },
-    {
-        name: "Blackburn Dotson",
-        email: "blackburndotson@furnigeer.com",
-        eyeColor: "brown",
-        friends: ["Jacklyn Lucas", "Linda Chapman"],
-        isActive: false,
-        balance: 1498,
-        skills: ["non", "amet", "ipsum"],
-        gender: "male",
-        age: 38,
-    },
-    {
-        name: "Sheree Anthony",
-        email: "shereeanthony@kog.com",
-        eyeColor: "brown",
-        friends: ["Goldie Gentry", "Briana Decker"],
-        isActive: true,
-        balance: 2764,
-        skills: ["lorem", "veniam", "culpa"],
-        gender: "female",
-        age: 39,
+        title: "Redder Than Blood",
+        author: "Tanith Lee",
+        genres: ["horror", "mysticism", "adventure"],
     },
 ];
+// Change code below this line
+const allGenres = books.flatMap(book => book.genres);
+const uniqueGenres = allGenres.filter((book, index, array) => array.indexOf(book) === index);
 
-const getUserEmails = users => {
-    return
-
-};
-
-console.log('11', getUserNames(users), ["Moore Hensley", "Sharlene Bush", "Ross Vazquez", "Elma Head", "Carey Barr", "Blackburn Dotson", "Sheree Anthony"]);
+console.log('11', allGenres, uniqueGenres,
+    ["adventure", "history", "fiction", "mysticism", "horror"]);

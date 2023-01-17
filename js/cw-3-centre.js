65c127ffed16552c5fc925d1f935305e839daa38
andrew samoilov <89135041+Andrew-Samoilov@users.noreply.github.com>
2023-01-17 22:16:37 +0200

    centre of attention
    in progres. make top & bottom borders

--- js/cw-3-centre.js
+++ js/cw-3-centre.js
@@ -1,55 +1,83 @@
 console.log(' -  * CodeWars * Centre of attention * - ');
 
 function central_pixels(img, colour) {
-
-    function drawImage(im = Image) {
-        console.log(`function drawImage. W=${im.width}, H=${im.height}, colour = ${colour}.`);
-        let tmpRes = [];
-        let numHeight = 1;
-        for (let i = 1; i <= im.pixels.length; i++) {
-            tmpRes.push(im.pixels[i - 1]);
-            // console.log(`i% i.width ${i% i.width}` );
-            if (i % im.width === 0) {
-                console.log(`${numHeight++}`, tmpRes);
-                tmpRes = [];
-            }
-        }
-    }
-
-    let res = [1];
-    let maxAttention = -2;
-    let pix = img.pixels;
+    let result = [];
+    let maxAttention = 0;
+    let pixels = img.pixels;
     drawImage(img);
 
-    function pixAtt(coord) { // функція для визначення глибини пікселя
-        for (let j = 0; j < pix.length; j++) {
-            /* дивимся по сторонам, циклом змінємо довжину погляду,
-               до pix.length не дійдемо, просте обмеження */
-
+function drawImage(image = Image) {
+    console.log(`function drawImage. W=${image.width}, H=${image.height}, colour = ${colour}.`);
+    let tmpRes = [];
+    let numberOfHeightLine = 1;
+    for (let i = 1; i <= image.pixels.length; i++) {
+        tmpRes.push(image.pixels[i - 1]);
+        // console.log(`i% i.width ${i% i.width}` );
+        if (i % image.width === 0) {
+            console.log(`${numberOfHeightLine++}`, tmpRes);
+            tmpRes = [];
         }
-
     }
+}
 
-    for (let i = 0; i < pix.length; i++) {
-        // console.log(`pix[${i}] ${pix[i]}`);
+    function pixAtt(coord) { // функція для визначення глибини пікселя
+        // console.log('coord',coord);
+    let pixelDeep = 0;
+        for (let j = coord; j <= pixels.length; j += 1) {
+            // console.log(`j`, j);
+        /* дивимся по сторонам, циклом змінємо довжину погляду,
+            до pixels.length не дійдемо, просте обмеження */
+
+        //fist value
+        // if ((j - 1) < 0) {        
+        //     pixelDeep = 1;
+        //     console.log(`fist value * coord`, coord, ` j =`, j, `j - 1 = ${j - 1}`, `pixelDeep = `, pixelDeep);
+        //     continue;
+        // }
+        
+        // top border
+        if (j - img.width < 0) {
+            pixelDeep = 1;
+            console.log('top border * coord', coord, ` j =`, j, `j - img.width - j = ${j - img.width}`, `pixelDeep = `, pixelDeep);      
+            return pixelDeep;
+        }
+       
+      // bottom border
+        if ((j + img.width) >= pixels.length) {
+            pixelDeep = 1;
+            console.log('bottom border * coord', coord, ` j =`, j, `j+img.width= ${j + img.width}`, `img.width = `, img.width);
+            return pixelDeep;
+        }
+        
+        
+    }
+    return pixelDeep;
+}
+
+    // Основний цикл, проходимося по пикселям
+    for (let i = 0; i < pixels.length; i++) {
+        // console.log(`pixels[${i}] ${pixels[i]}`);
 
         // cпівпадає з тим, шо шукаємо, робимо
-        if (pix[i] === colour) {
-            console.log(` pix[${i}] = colour| ${colour}  pixAtt(${i}) ${pixAtt(i)} `);
-
-
-
-
-
-        } else {
-            pix[i] = 0;
+        if (pixels[i] === colour) {
+            // console.log(` pixels[${i}] = colour| ${colour}  pixAtt(${i}) ${pixAtt(i)} `);
+            if (pixAtt(i) > maxAttention) {      
+                result.push(i); //додаємо в результат індекс масива
+            }
+            
+            // maxAttention++;
+            // console.log('maxAttention', maxAttention);
         }
 
-
     }
-    // drawImage(pix);
-    console.log(...res);
-    return Math.max(...pix);
+    // drawImage(pixels);
+    console.log(...result);
+    if (!result.length) {
+        // console.log(`!!!`);
+        return `!! There are no pixels with colour `+colour;   
+    }
+    // return Math.max(...result);
+    return result;
 }
 
 class Image {
@@ -64,11 +92,11 @@ let ascending = (a, b) => a - b;
 
 let picture = new Image(
     [1, 1, 4, 4, 4, 4, 2, 2, 2, 2, // prettier-ignore
-        1, 1, 1, 1, 2, 2, 2, 2, 2, 2,
-        1, 1, 1, 1, 2, 2, 2, 2, 2, 2,
-        1, 1, 1, 1, 1, 3, 2, 2, 2, 2,
-        1, 1, 1, 1, 1, 3, 3, 3, 2, 2,
-        1, 1, 1, 1, 1, 1, 3, 3, 3, 3], 10, 6);
+     1, 1, 1, 1, 2, 2, 2, 2, 2, 2,
+     1, 1, 1, 1, 2, 2, 2, 2, 2, 2,
+     1, 1, 1, 1, 1, 3, 2, 2, 2, 2,
+     1, 1, 1, 1, 1, 3, 3, 3, 2, 2,
+     1, 1, 1, 1, 1, 1, 3, 3, 3, 3], 10, 6);
 let imag;
 let picture2 = new Image(
     [1, 1, 1, 1, 1, 1, 1, 1, 2, 1, // prettier-ignore
@@ -107,16 +135,22 @@ let picture4 = new Image(// prettier-ignore
         5, 5, 5, 5, 6, 6, 7, 7, 7, 7, 5, 5, 7, 7, 5, 5, 5, 5, 5, 5, 5, 6, 6, 7, 7, 7, 7, 7, 7,
         7, 7, 7, 7, 6, 6, 6, 6, 6, 6, 6, 6, 5, 5, 5, 5, 5, 6, 6, 7, 7, 7, 7, 6, 6, 7, 7, 7, 7,
         7, 7, 7, 7, 6, 6, 6, 6, 6, 6, 6, 6, 5, 5, 5, 5, 5, 6, 6, 7, 7, 7, 7, 6, 6, 7, 7, 7, 7], 29, 27);
-let picture5 = new Image(
+
+let picture5 = new Image(// prettier-ignore
     [8, 8,
-        7, 8], 2, 2);
+     7, 8], 2, 2);
 
 imag = picture;
+console.log(central_pixels(imag, 2), `2`);
 
-// console.log(central_pixels(imag, 2), `!!`);
+
+// imag = picture2;
+// console.log(central_pixels(imag, 1), `!&?`);
+
 // Only one red pixel has the maximum depth of 3:
-let red_ctr = [32];
-console.log(central_pixels(imag, 1), red_ctr);
+// let red_ctr = [32];
+// console.log(central_pixels(red_ctr, 1), red_ctr);
+
 // // Multiple blue pixels have the maximum depth of 2:
 // let blue_ctr = [16, 17, 18, 26, 27, 28, 38];
 // console.log(central_pixels(imag, 2), blue_ctr);
@@ -137,25 +171,26 @@ console.log(central_pixels(imag, 1), red_ctr);
 // image.pixels[32] = 3;
 // let new_ctr = [ 11,21,41,43 ];
 // console.log(central_pixels(imag, 1).sort(ascending), new_ctr);
+
 /* look left
-if (img.pixels[i - 1] === undefined || pix[i - 1] === 0) {
+if (img.pixels[i - 1] === undefined || pixels[i - 1] === 0) {
     console.log(`- 1[${i - 1}]=u`);
-    pix[i] = 1;
+    pixels[i] = 1;
 }
 // look right
-if (img.pixels[i + 1] === undefined || pix[i + 1] === 0) {
+if (img.pixels[i + 1] === undefined || pixels[i + 1] === 0) {
     console.log(`+ 1[${i + 1}]=u`);
-    pix[i] = 1;
+    pixels[i] = 1;
 }
 // look top
-if (img.pixels[i - img.width] === undefined || pix[i - img.width] === 0) {
+if (img.pixels[i - img.width] === undefined || pixels[i - img.width] === 0) {
     console.log(`img.pixels[${i} - ${img.width}] ${img.pixels[i - img.width]}`);
-    pix[i] = 1;
+    pixels[i] = 1;
 }
 // look down
-if (img.pixels[i + img.width] === undefined || pix[i + img.width] === 0) {
+if (img.pixels[i + img.width] === undefined || pixels[i + img.width] === 0) {
     console.log(`img.pixels[${i} + ${img.width}] ${img.pixels[i + img.width]}`);
-    pix[i] = 1;*/
+    pixels[i] = 1;*/
 
 /*    старий варіант умов (з 1 )
 
@@ -163,42 +198,42 @@ if (img.pixels[i] === colour) {
 
     if (
         img.pixels[i - 1] === undefined || //left
-        pix[i - 1] === 0 ||
+        pixels[i - 1] === 0 ||
         img.pixels[i + 1] === undefined || //right
-        pix[i + 1] === 0 ||
+        pixels[i + 1] === 0 ||
         img.pixels[i + 1] != colour ||
         img.pixels[i - img.width] === undefined || //top
-        pix[i - img.width] === 0 ||
+        pixels[i - img.width] === 0 ||
         img.pixels[i + img.width] === undefined || //bottom
         img.pixels[i + img.width] != colour ||
-        pix[i + img.width] === 0
+        pixels[i + img.width] === 0
     ) {
-        pix[i] = 1;
+        pixels[i] = 1;
     }
 
 
 } else {
-    pix[i] = 0;
+    pixels[i] = 0;
 }*/
 /*
-for (let j = 0; j < pix.length; j++) {
+for (let j = 0; j < pixels.length; j++) {
     /* дивимся по сторонам, циклом змінємо довжину погляду,
-               до pix.length не дійдемо, просте обмеження 
+               до pixels.length не дійдемо, просте обмеження 
     if (
-        pix[i - j] === undefined || //left
-        pix[i - j] === 0 ||
-        pix[i - j] != colour ||
-        pix[i + j] === undefined || //right
-        pix[i + j] === 0 ||
-        pix[i + j] != colour ||
-        pix[i - img.width * j] === undefined || //top
-        pix[i - img.width * j] === 0 ||
-        pix[i - img.width * j] != colour ||
-        pix[i + img.width * j] === undefined || //bottom
-        pix[i + img.width * j] === 0 ||
-        pix[i + img.width * j] != colour
+        pixels[i - j] === undefined || //left
+        pixels[i - j] === 0 ||
+        pixels[i - j] != colour ||
+        pixels[i + j] === undefined || //right
+        pixels[i + j] === 0 ||
+        pixels[i + j] != colour ||
+        pixels[i - img.width * j] === undefined || //top
+        pixels[i - img.width * j] === 0 ||
+        pixels[i - img.width * j] != colour ||
+        pixels[i + img.width * j] === undefined || //bottom
+        pixels[i + img.width * j] === 0 ||
+        pixels[i + img.width * j] != colour
     ) {
-        pix[i] = j;
+        pixels[i] = j;
 
         if (maxAttention < j) {
             maxAttention = j;
@@ -215,3 +250,9 @@ for (let j = 0; j < pix.length; j++) {
         break;
     }
 }*/
+
+console.log(` - * Worked test * -`);
+// зробив щоб при відсутності кольору писало повідомлення
+imag = picture5;
+console.log(central_pixels(imag, 2), `!!There are no pixels with colour 2`);
+    

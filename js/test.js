ab1afba43809352ac36c33657a70f0e8ea0d2b93
andrew samoilov <89135041+Andrew-Samoilov@users.noreply.github.com>
2023-01-18 11:09:31 +0200

    find eval function

--- js/test.js
+++ js/test.js
@@ -1,3 +1,11 @@
 console.log(' -  * test * - '); // console.log(' -  * CodeWars *  * - ');
 
-console.log(' hi from new NoteBook');
+let expression = 'index===5';
+
+console.log(`expression `, expression);
+for (let index = 0; index < 10; index++) {
+    if (eval(expression)) {
+        console.log(`expression works !`, expression);
+    }
+    
+}

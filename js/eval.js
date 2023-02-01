ee73c2cee4bb4d51d5ca66214b4c21d6d056ac6d
andrew samoilov <89135041+Andrew-Samoilov@users.noreply.github.com>
2023-02-02 00:13:16 +0200

    JS Project day1

---
+++ js/eval.js
@@ -0,0 +1,11 @@
+console.log(' -  * test * - '); // console.log(' -  * CodeWars *  * - ');
+
+let expression = 'index===5';
+
+console.log(`expression `, expression);
+for (let index = 0; index < 10; index++) {
+    if (eval(expression)) {
+        console.log(`expression works !`, expression);
+    }
+
+}
--- js/test.js
+++ js/test.js
@@ -1,11 +1,31 @@
-console.log(' -  * test * - '); // console.log(' -  * CodeWars *  * - ');
+console.log(' -  * test * - '); 
 
-let expression = 'index===5';
+let mainBtn = document.getElementById('main_btn');
+mainBtn.addEventListener('click', Load);
 
-console.log(`expression `, expression);
-for (let index = 0; index < 10; index++) {
-    if (eval(expression)) {
-        console.log(`expression works !`, expression);
-    }
-    
+const API_KEY = '50540b41e66ef631d8d57e13679f9024';
+const TRENDING_TIME = 'week';
+
+function Load(){
+    console.log('clickBtn');
+
+    const fetchMovies = async () => {
+        const response = await fetch('https://api.themoviedb.org/3/trending/all/' +
+            TRENDING_TIME + '?api_key=' + API_KEY);
+        const movies = await response.json();
+        return movies;
+    };
+
+    fetchMovies()
+        .then(movies => {
+            console.log(movies.results);
+            localStorage.setItem('currentPageMuvie', JSON.stringify(movies.results));
+        })
+        .catch(error => console.log(error));
+
 }
+
+//https://api.themoviedb.org/3/trending/all/day?api_key=<<api_key>>
+
+// конкретний фільм 
+// https://api.themoviedb.org/3/movie/550?api_key=50540b41e66ef631d8d57e13679f9024

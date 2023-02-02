bbdac4fd6d8a193a6aacb6aaa0cfa34bae54472f
andrew samoilov <89135041+Andrew-Samoilov@users.noreply.github.com>
2023-02-02 20:40:53 +0200

    ++

--- js/test.js
+++ js/test.js
@@ -64,10 +64,20 @@ function Trending(){
 }
 
 
-function Render(m) {
-    // console.log('Render', m);
-searchBtn.insertAdjacentHTML("afterend",m)
-
+function Render(muvies) {
+    let arrayMuvies = JSON.parse(muvies);
+    console.log('Render', arrayMuvies);
+    let resultHtml = [];
+    searchBtn.insertAdjacentHTML("afterend", '');
+    for (const muvie of arrayMuvies) {
+        resultHtml.push('<li>Title ' + muvie.title + '</li>');   
+        // resultHtml.push('<img src="' + muvie.poster_path + '" alt="'+ muvie.title +'"></li>');
+        resultHtml.push('<li>genre_ids ' + muvie.genre_ids + '</li>');
+        resultHtml.push('<li>vote_average ' + muvie.vote_average + '</li>');
+        
+    }
+    // console.log('Afrter  Render', resultHtml.join(''));
+    searchBtn.insertAdjacentHTML("afterend", resultHtml.join(''));
 }
 
 //https://api.themoviedb.org/3/trending/all/day?api_key=<<api_key>>
